dc8d3d9ebc0fa0fa321bde51523f131a35c4b610
den4day <den4day@gmail.com>
2023-03-22 03:31:56 +0300

    addRoomsFilter

--- view/pages/home/script.js
+++ view/pages/home/script.js
@@ -3,6 +3,30 @@ let sock = io();
 let temp = document.querySelector('.weather__tempIn');
 let hum = document.querySelector('.weather__hum');
 
+let tabsBtn = document.querySelectorAll('.control__item');
+let tabsTile = document.querySelectorAll('.control__tile');
+
+
+tabsBtn.forEach(item =>
+    item.addEventListener('click', () => {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelectorAll(".group" + tabId);
+
+        if (!currentBtn.classList.contains('control__item--active')) {
+            tabsBtn.forEach(item => item.classList.remove('control__item--active'));
+            tabsTile.forEach(item => item.classList.remove('control__tile--active'));
+
+
+            currentBtn.classList.add('control__item--active');
+            setTimeout(() => currentTab.forEach(item => item.classList.add('control__tile--active')), 200);
+        }
+    })
+);
+
+document.querySelector(".control__item").click();
+
+
 // обработчик события status
 sock.on('status', data => {
     if (data) {
@@ -19,6 +43,6 @@ sock.on('status', data => {
 })
 
 // обновляем статус оп таймеру 1 раз в секунду
-setInterval(() => {
-    sock.emit('status');
-}, 2000);
+// setInterval(() => {
+//     sock.emit('status');
+// }, 2000);
