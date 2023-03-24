356260c4b946fe8aff28e47d7534638050d46509
den4day <den4day@gmail.com>
2023-03-24 12:28:39 +0300

    fix

--- index.js
+++ index.js
@@ -52,5 +52,6 @@ io.on('connect', socket => {
     console.log('new user connected');
     socket.on("data", data => {
         console.log(data);
+        socket.emit("sendToHomePage", data);
     });
 });
--- view/pages/home/script.js
+++ view/pages/home/script.js
@@ -1,13 +1,8 @@
-let sock = io();
-
-let temp = document.querySelector('.weather__tempIn');
-let hum = document.querySelector('.weather__hum');
+// let temp = document.querySelector('.weather__tempIn');
+// let hum = document.querySelector('.weather__hum');
 
 // let tabsBtn = document.querySelectorAll('.control__item');
 // let tabsTile = document.querySelectorAll('.control__tile');
-
-// let btnToggle = document.getElementById('btn-toggle');
-// var loadEnabled = 0;
 
 // tabsBtn.forEach(item =>
 //     item.addEventListener('click', () => {
@@ -19,42 +14,16 @@ let hum = document.querySelector('.weather__hum');
 //             tabsBtn.forEach(item => item.classList.remove('control__item--active'));
 //             tabsTile.forEach(item => item.classList.remove('control__tile--active'));
 
-
 //             currentBtn.classList.add('control__item--active');
 //             setTimeout(() => currentTab.forEach(item => item.classList.add('control__tile--active')), 200);
 //         }
 //     })
 // );
 
-document.querySelector(".control__item").click();
+// document.querySelector(".control__item").click();
 
-// обработчик события status
-sock.on('status', data => {
-    if (data) {
-        // если пришли данные температуры
-        if (data.hasOwnProperty('t')) {
-            // выводим ее
-            temp.innerHTML = data.t;
-        }
-        // влажность
-        if (data.hasOwnProperty('h')) {
-            hum.innerHTML = data.h;
-        }
-        // статус нашего переключателя
-        if (data.hasOwnProperty('l')) {
-            loadEnabled = parseInt(data.l)
-            fieldL.innerHTML = loadEnabled ? 'ВКЛ.' : 'ВЫКЛ.';
+let socket = io("http://localhost:3000");
 
-            // кнопка для изменения состояния
-            btnToggle.innerHTML = loadEnabled ? 'ВЫКЛ.' : 'ВКЛ.';
-            btnToggle.removeAttribute('disabled');
-        }
-    }
-});
+socket.on("sendToHomePage", data => console.log(data));
 
 
-
-// обновляем статус оп таймеру 1 раз в секунду
-// setInterval(() => {
-//     sock.emit('status');
-// }, 2000);
