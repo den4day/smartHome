c68b4313bbd0a80dc5c2aa65c33c2a6639cbcdf8
DoomGuy1818 <zasdqs745@gmail.com>
2023-03-24 12:18:43 +0300

    231

--- view/pages/home/script.js
+++ view/pages/home/script.js
@@ -1,5 +1,3 @@
-let sock = io();
-
 let temp = document.querySelector('.weather__tempIn');
 let hum = document.querySelector('.weather__hum');
 
@@ -27,34 +25,3 @@ tabsBtn.forEach(item =>
 );
 
 document.querySelector(".control__item").click();
-
-// обработчик события status
-sock.on('status', data => {
-    if (data) {
-        // если пришли данные температуры
-        if (data.hasOwnProperty('t')) {
-            // выводим ее
-            temp.innerHTML = data.t;
-        }
-        // влажность
-        if (data.hasOwnProperty('h')) {
-            hum.innerHTML = data.h;
-        }
-        // статус нашего переключателя
-        if (data.hasOwnProperty('l')) {
-            loadEnabled = parseInt(data.l)
-            fieldL.innerHTML = loadEnabled ? 'ВКЛ.' : 'ВЫКЛ.';
-
-            // кнопка для изменения состояния
-            btnToggle.innerHTML = loadEnabled ? 'ВЫКЛ.' : 'ВКЛ.';
-            btnToggle.removeAttribute('disabled');
-        }
-    }
-});
-
-
-
-// обновляем статус оп таймеру 1 раз в секунду
-// setInterval(() => {
-//     sock.emit('status');
-// }, 2000);
---
+++ view/pages/light/script.js
@@ -0,0 +1,8 @@
+// const socket = io("ws://localhost:3000");
+
+
+// socket.on('greet', data => {
+//     console.log(data);
+// });
+
+// socket.emit('message', 'some data from client =)');
